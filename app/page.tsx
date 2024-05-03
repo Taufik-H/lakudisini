f703bee2fe32452e7f2d32a77250eee8a37f5b5d
Taufik-H <taufikrpl1@gmail.com>
2024-05-03 13:33:23 +0700

    sticky nav blur

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,13 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <Hero />
+      <div className="mx-auto my-20">
+        <h1 className="text-center text-4xl font-bold">DEVELOPING</h1>
+        <p className="mt-5 text-center">
+          Website ini sedang dalam tahap pengembangan. Terima kasih atas
+          pengertian Anda.🙏
+        </p>
+      </div>
     </>
   );
 }
