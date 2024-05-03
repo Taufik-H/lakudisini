9ce0631087db3884ce76f5367c011cb11c2fb489
Taufik-H <taufikrpl1@gmail.com>
2024-05-03 10:31:29 +0700

    hero mobile check, yet Tablet

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -8,14 +8,13 @@ import { BorderAnimate } from "../moving-border";
 const Hero = () => {
   return (
     <div className="max-container ">
-      <div className="my-auto flex h-screen w-full  justify-between">
-        <div className="padding-container flex w-full flex-col items-start justify-center gap-2">
-          <div className="mt-10 flex items-center gap-3 rounded-full bg-blue-100 px-3 py-2 text-sm font-medium text-blue-700 dark:border-l dark:border-t dark:border-slate-500 dark:bg-blue-100 dark:bg-opacity-20 dark:text-white">
+      <div className=" mt-20 flex h-screen w-full flex-col justify-between gap-20 lg:my-auto lg:mt-0 lg:flex-row  lg:gap-0">
+        <div className="padding-container flex w-full flex-col items-center  justify-center gap-2 lg:items-start">
+          <div className="mt-10 flex items-center justify-center gap-3 rounded-full bg-blue-100 px-3 py-2 text-sm font-medium text-blue-700 dark:border-l dark:border-t dark:border-slate-500 dark:bg-blue-100 dark:bg-opacity-20 dark:text-white lg:justify-start">
             <p>Lakudisini yang penting laku</p>
-            {/* <Image src={"/images/Bag.svg"} height={25} width={25} alt="bag" /> */}
             <ShoppingBag size={20} />
           </div>
-          <h1 className="text-6xl font-black leading-normal text-slate-900 dark:text-white">
+          <h1 className="text-center text-5xl font-black leading-snug text-slate-900 dark:text-white lg:text-left lg:text-6xl">
             Omset Terlebih Dahulu, Bayar{" "}
             <span className="bg-gradient-to-tr from-blue-700 to-cyan-200 bg-clip-text text-transparent">
               Kemudian!
@@ -23,12 +22,15 @@ const Hero = () => {
           </h1>
           {/* tombol */}
           <div className="mt-10 flex items-center gap-3">
-            <Button size={"lg"} className="bg-blue-700 text-white">
+            <Button
+              size={"lg"}
+              className=" bg-blue-700 text-white hover:bg-blue-900"
+            >
               Layanan kami
             </Button>
             <Link href={"#"}>
               <Button
-                className="border-2 border-slate-900 p-2 dark:border-slate-200"
+                className="border-2 border-slate-900 p-2 dark:border-slate-200 dark:hover:bg-slate-900"
                 size={"lg"}
                 variant={"outline"}
               >
@@ -58,12 +60,12 @@ const Hero = () => {
               height={150}
               width={150}
               alt="hero image"
-              className="animate-spin-slow absolute left-[-70px] top-[50px] rounded-full border-8 border-white dark:border-background"
+              className="animate-spin-slow absolute  left-[120px] top-[-50px] w-28 rounded-full border-8 border-white dark:border-background lg:left-[-70px] lg:top-[50px] lg:w-auto"
             />
-            <div className="absolute bottom-[-30px] right-[-30px]">
+            <div className="absolute bottom-[-30px] right-[70px]  lg:right-[-30px]">
               <BorderAnimate
                 borderRadius="10px"
-                className="border-neutral-200 bg-white text-black shadow-lg dark:border-slate-800 dark:bg-slate-900 dark:text-white"
+                className="border-neutral-200  bg-white text-black shadow-lg dark:border-slate-800 dark:bg-slate-900 dark:text-white"
               >
                 <div className="flex   items-center  justify-center gap-3 px-5 py-3  font-semibold capitalize  ">
                   <div className="flex flex-col items-start">
