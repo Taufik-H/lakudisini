068d9bf842e02ecdeb8c23a3beabf88e19e27af0
Taufik-H <taufikrpl1@gmail.com>
2024-05-03 13:35:31 +0700

    on development

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <div className="mx-auto my-20">
+      <div className="flex h-screen flex-col items-center justify-center">
         <h1 className="text-center text-4xl font-bold">DEVELOPING</h1>
         <p className="mt-5 text-center">
           Website ini sedang dalam tahap pengembangan. Terima kasih atas
