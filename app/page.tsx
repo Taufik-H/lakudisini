b61b4088af3ba7d5f49a9377cc1071f047373625
Taufik-H <taufikrpl1@gmail.com>
2024-05-03 17:43:46 +0700

    feat:Divider

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Divider from "@/components/sections/divider";
 import Hero from "@/components/sections/hero";
 import Marque from "@/components/sections/marque";
 import TentangKami from "@/components/sections/tentang-kami";
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Marque />
       <TentangKami />
+      <Divider />
       <div className="flex h-screen flex-col items-center justify-center">
         <h1 className="text-center text-4xl font-bold">DEVELOPING</h1>
         <p className="mt-5 text-center">
---
+++ components/sections/divider.tsx
@@ -0,0 +1,17 @@
+import { ChevronDown } from "lucide-react";
+import Image from "next/image";
+import React from "react";
+
+const Divider = () => {
+  return (
+    <div className="max-container padding-container my-10  items-center justify-center gap-3">
+      <div className="w-full bg-slate-300 p-[1px] dark:bg-blue-700"></div>
+      <div className="flex items-center justify-center rounded-full border border-input bg-white  p-1 shadow-lg  dark:bg-blue-700 ">
+        <ChevronDown className="text-blue-700 dark:text-white" />
+      </div>
+      <div className="w-full bg-slate-300 p-[1px] dark:bg-blue-700"></div>
+    </div>
+  );
+};
+
+export default Divider;
